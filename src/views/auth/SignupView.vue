33b4bb9f705e519e353e5fd53d73a7e964af4dd4
sharif <sharif.jatri@gmail.com>
2022-08-11 15:19:14 +0600

    user registration  functionality

--- src/views/auth/SignupView.vue
+++ src/views/auth/SignupView.vue
@@ -1,17 +1,43 @@
 <template>
   <h1>Sign Up</h1>
-  <div class="form-container">
-    <input type="text" placeholder="Enter Name"/>
-    <input type="email" placeholder="Enter Email"/>
-    <input type="password" placeholder="Enter Password"/>
-    <button>Register</button>
-    <router-link :to="{name: 'login'}">Login</router-link>
-  </div>
+    <form class="form-container" @submit.prevent="formHandler">
+      <input type="text" v-model="formData.name" placeholder="Enter Name"/>
+      <input type="email" v-model="formData.email" placeholder="Enter Email"/>
+      <input type="password" v-model="formData.password" placeholder="Enter Password"/>
+      <input type="submit" placeholder="Submit">
+  </form>
+  <router-link :to="{name: 'login'}">Login</router-link>
 </template>
 
 <script>
+import {reactive} from "vue";
+import axios from "axios";
+
 export default {
   name: 'SignupView',
-
+  setup() {
+    const formData = reactive({
+      name: null,
+      email: null,
+      password: null,
+    });
+    const formHandler = async () => {
+      if (formData.name && formData.email && formData.password) {
+        const result = await axios.post(process.env.VUE_APP_API+"users", formData);
+        if(result.status === 201){
+          formData.name = null
+          formData.email = null
+          formData.password = null
+          alert('registration successful')
+        }
+      } else {
+        alert('Please input all values')
+      }
+    }
+    return{
+      formData,
+      formHandler
+    }
+  }
 }
 </script>
